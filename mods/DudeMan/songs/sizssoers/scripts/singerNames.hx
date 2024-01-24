8f4f8d0a223bc8aec2ce84ed07095b25edd1c936
NeonExists <91714683+NeonExists@users.noreply.github.com>
2024-01-24 16:53:14 -0600

    Singer Names !
    only works in RSVP and Pastel Fever 24 atm, gonna add to other songs later !

---
+++ mods/DudeMan/songs/sizssoers/scripts/singerNames.hx
@@ -0,0 +1,46 @@
+function postCreate() {
+
+	opponent = new FlxSprite(125, 800).loadGraphic(Paths.image('singerNames/GhosttaleSans'));
+	opponent.antialiasing = false;
+	opponent.scrollFactor.set(1, 1);
+	opponent.scale.x = 2;
+	opponent.scale.y = 2;	
+	opponent.alpha = 1;
+	opponent.cameras = [camHUD];
+	opponent.updateHitbox();
+	add(opponent);
+
+	vs = new FlxSprite(450, 800).loadGraphic(Paths.image('singerNames/VS'));
+	vs.antialiasing = false;
+	vs.scrollFactor.set(1, 1);
+	vs.scale.x = 2;
+	vs.scale.y = 2;	
+	vs.alpha = 1;
+	vs.cameras = [camHUD];
+	vs.updateHitbox();
+	add(vs);
+
+	playerName = new FlxSprite(800, 800).loadGraphic(Paths.image('singerNames/Dax'));
+	playerName.antialiasing = false;
+	playerName.scrollFactor.set(1, 1);
+	playerName.scale.x = 2;
+	playerName.scale.y = 2;	
+	playerName.alpha = 1;
+	playerName.cameras = [camHUD];
+	playerName.updateHitbox();
+	insert(50,playerName);
+
+}
+
+function stepHit(curStep:Int) { 
+    switch (curStep) {
+        case 16:
+            FlxTween.tween(playerName, {y: 100}, 3, {ease:FlxEase.quartOut});	    
+            FlxTween.tween(opponent, {y: 100}, 3, {ease:FlxEase.quartOut});	 
+            FlxTween.tween(vs, {y: 100}, 3, {ease:FlxEase.quartOut});	
+        case 76:
+            FlxTween.tween(playerName, {y: 800}, 2, {ease:FlxEase.quartIn});	    
+            FlxTween.tween(opponent, {y: 800}, 2, {ease:FlxEase.quartIn});	 
+            FlxTween.tween(vs, {y: 800}, 2, {ease:FlxEase.quartIn});
+    }
+}
