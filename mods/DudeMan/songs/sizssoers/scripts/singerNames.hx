17edaaf6aac0a7b88a5288561f2598d32bbb32b4
NeonExists <91714683+NeonExists@users.noreply.github.com>
2024-01-24 20:37:22 -0600

    singerNames 2

--- mods/DudeMan/songs/sizssoers/scripts/singerNames.hx
+++ mods/DudeMan/songs/sizssoers/scripts/singerNames.hx
@@ -1,46 +1,63 @@
 function postCreate() {
 
-	opponent = new FlxSprite(125, 800).loadGraphic(Paths.image('singerNames/GhosttaleSans'));
+	opponent = new FlxSprite(375, 1200).loadGraphic(Paths.image('singerNames/GhosttaleSans'));
 	opponent.antialiasing = false;
 	opponent.scrollFactor.set(1, 1);
-	opponent.scale.x = 2;
-	opponent.scale.y = 2;	
+	opponent.scale.x = 1.2;
+	opponent.scale.y = 1.2;	
 	opponent.alpha = 1;
 	opponent.cameras = [camHUD];
 	opponent.updateHitbox();
 	add(opponent);
 
-	vs = new FlxSprite(450, 800).loadGraphic(Paths.image('singerNames/VS'));
+	vs = new FlxSprite(450, 1200).loadGraphic(Paths.image('singerNames/VS'));
 	vs.antialiasing = false;
 	vs.scrollFactor.set(1, 1);
-	vs.scale.x = 2;
-	vs.scale.y = 2;	
-	vs.alpha = 1;
+	vs.scale.x = 1.2;
+	vs.scale.y = 1.2;
+	vs.screenCenter();	
+	vs.alpha = 0;
 	vs.cameras = [camHUD];
 	vs.updateHitbox();
 	add(vs);
 
-	playerName = new FlxSprite(800, 800).loadGraphic(Paths.image('singerNames/Dax'));
+	playerName = new FlxSprite(700, 1200).loadGraphic(Paths.image('singerNames/Dax'));
 	playerName.antialiasing = false;
 	playerName.scrollFactor.set(1, 1);
-	playerName.scale.x = 2;
-	playerName.scale.y = 2;	
+	playerName.scale.x = 1.2;
+	playerName.scale.y = 1.2;	
 	playerName.alpha = 1;
 	playerName.cameras = [camHUD];
 	playerName.updateHitbox();
-	insert(50,playerName);
+	add(playerName);
+
+	thisisyou = new FlxSprite(750, 1400).loadGraphic(Paths.image('singerNames/UPSCROLLthanksStar'));
+	thisisyou.antialiasing = false;
+	thisisyou.scrollFactor.set(1, 1);
+	thisisyou.scale.x = 0.6;
+	thisisyou.scale.y = 0.6;	
+	thisisyou.alpha = 1;
+	thisisyou.cameras = [camHUD];
+	thisisyou.updateHitbox();
+	add(thisisyou);
+
+	vs.y = 1200;
 
 }
+
 
 function stepHit(curStep:Int) { 
     switch (curStep) {
         case 16:
-            FlxTween.tween(playerName, {y: 100}, 3, {ease:FlxEase.quartOut});	    
-            FlxTween.tween(opponent, {y: 100}, 3, {ease:FlxEase.quartOut});	 
-            FlxTween.tween(vs, {y: 100}, 3, {ease:FlxEase.quartOut});	
+	    vs.alpha = 1;
+            FlxTween.tween(playerName, {y: 250}, 3, {ease:FlxEase.quartOut});	    
+            FlxTween.tween(opponent, {y: 250}, 3, {ease:FlxEase.quartOut});	 
+            FlxTween.tween(vs, {y: 250}, 3, {ease:FlxEase.quartOut});	
+            FlxTween.tween(thisisyou, {y: 450}, 3, {ease:FlxEase.quartOut});	
         case 76:
-            FlxTween.tween(playerName, {y: 800}, 2, {ease:FlxEase.quartIn});	    
-            FlxTween.tween(opponent, {y: 800}, 2, {ease:FlxEase.quartIn});	 
-            FlxTween.tween(vs, {y: 800}, 2, {ease:FlxEase.quartIn});
+            FlxTween.tween(playerName, {y: 1200}, 2, {ease:FlxEase.quartIn});	    
+            FlxTween.tween(opponent, {y: 1200}, 2, {ease:FlxEase.quartIn});	 
+            FlxTween.tween(vs, {y: 1200}, 2, {ease:FlxEase.quartIn});
+            FlxTween.tween(thisisyou, {y: 1200}, 2, {ease:FlxEase.quartIn});
     }
 }
