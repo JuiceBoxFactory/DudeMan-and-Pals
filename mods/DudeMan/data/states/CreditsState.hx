66e906390fcc45b5ab591f4b067739c46909c655
NeonExists <mehbeingrandom@gmail.com>
2024-02-10 11:33:16 -0600

    minor fixes

--- mods/DudeMan/data/states/CreditsState.hx
+++ mods/DudeMan/data/states/CreditsState.hx
@@ -79,61 +79,52 @@ function update() {
 	if (FlxG.keys.justPressed.ENTER) {
 		CoolUtil.openURL(links[Selected]);
 	}
-	if (Selected == 0) {
+	switch(Selected) {
+	case 0:
 		devname.text = 'MeltyKelpy';
 		thingsdone.text = 'Artist\nCoder\nCharter\nDirector ig\nVA\n\n"soup, if you dont like these sprites Ill kill myself"';
-	}
-	if (Selected == 1) {
+	case 1:
 		devname.text = 'NullFrequency';
 		thingsdone.text = 'Musician\nCoder\n\n\n\n\n"Roses are red, violets are blue, Im going to KILL YOU!!!!!"';
-	}
-	if (Selected == 2) {
+	case 2:
 		devname.text = 'AussieDoesThings';
 		thingsdone.text = 'Musician\nVA\nChromatic Maker\n\n\n\n"thats not a cloud, were in someones balls!"';
-	}
-	if (Selected == 3) {
+	case 3:
 		devname.text = 'GhostyBrick';
-		thingsdone.text = 'Charter\nMade DudeMan (The Character)\n\n\n\n"if my hurt didnt throat so much"';
-	}
-	if (Selected == 4) {
+		thingsdone.text = 'Artist\nCharter\nMade DudeMan (The Character)\n\n\n"if my hurt didnt throat so much"';
+	case 4:
 		devname.text = 'kosejumpscare';
 		thingsdone.text = 'Musician\nVA\n\n\n\n\n"Its unbelievable how much incest anime I know"';
-	}
-	if (Selected == 5) {
+	case 5:
 		devname.text = 'CherriBlossom';
 		thingsdone.text = 'Artist\nVA\n\n\n\n\n"IS THAT MY commission"';
-	}
-	if (Selected == 6) {
+	case 6:
 		devname.text = 'Antydote';
 		thingsdone.text = 'VA (of themself)\nBurger King Employee\n\n\n\n\n"HOLY SHIT IS THAT DX FROM GAMEBREAK- Du- OH MY- that should be my qoute"';
-	}
-	if (Selected == 7) {
+	case 7:
 		devname.text = 'Missy';
 		thingsdone.text = 'Artist\n\n\n\n\n\n"missing: i scaM KIDS  so i can feedmy gambling addictiion"';
-	}
-	if (Selected == 8) {
+	case 8:
 		devname.text = 'R3t1xTheIDIOT';
 		thingsdone.text = 'Musician\nCoder\n\n\n\n\n"god dammit ghost, the world doesnt need to know about how i fill my holes. kms"';
-	}
-	if (Selected == 9) {
+	case 9:
 		devname.text = 'CazTheClown';
 		thingsdone.text = 'Artist\n\n\n\n\n\n"Man- just.. oh my fucking god give me a sec, I just murdered a family of 4"';
-	}
-	if (Selected == 10) {
+	case 10:
 		devname.text = 'Saayo';
 		thingsdone.text = 'Artist\nVA\nChromatic Maker\n\n\n\n"booty thick"';
-	}
-	if (Selected == 11) {
+	case 11:
 		devname.text = 'Angeltheboi';
 		thingsdone.text = 'Charter\nVA\n\n\n\n\n"I smeared doodoo all over the wall"';
-	}
-	if (Selected == 12) {
+	case 12:
 		devname.text = 'That1Gamer';
 		thingsdone.text = 'Artist\n\n\n\n\n\n"fuck my heterosexuality"';
-	}
-	if (Selected == 13) {
+	case 13:
 		devname.text = 'Lucidric';
 		thingsdone.text = 'Artist\nCoded Website\n\n\n\n\n"im gonna cut open my scrotum and juggle my testicles"';
+	default:
+		devname.text = 'this person doesnt exist';
+		thingsdone.text = 'my penis is so big :3';
 	}
 	if (Selected > 13) {
 		Selected = 0;
