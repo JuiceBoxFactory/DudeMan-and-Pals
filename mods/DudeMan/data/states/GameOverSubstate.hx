df5c5440555e253ced7578481c250c4b725ba29e
NeonExists <mehbeingrandom@gmail.com>
2024-02-09 19:49:03 -0600

    fixed gameover
    you have a dream, you wish to tell, and its just laughable, but hey kid, what the hell ! you're one of a kind ! a charming demon belle. NOW lets give these burning FOOLS a place to quell, take it boys ! inside of every demon is a lost cause

--- mods/DudeMan/data/states/GameOverSubstate.hx
+++ mods/DudeMan/data/states/GameOverSubstate.hx
@@ -11,10 +11,23 @@ function create() {
 	deathQoutes.loadGraphic(Paths.image('deathScreens/'+PlayState.SONG.meta.displayName));
 	deathQoutes.cameras = [deathCam];
 	add(deathQoutes);
+
+	fuckyou = new FlxSprite(0, 0);
+	fuckyou.scrollFactor.set(0, 0);
+	fuckyou.loadGraphic(Paths.image('deathScreens/recovery'));
+	fuckyou.cameras = [deathCam];
+	fuckyou.alpha = 0;
+	add(fuckyou);
+
 }
 
 function update() {
 
+
+	if (controls.ACCEPT) {
+		FlxTween.tween(fuckyou, {alpha: 1}, 2);
+	}
+	
 	character.alpha = 0;
 
 }
