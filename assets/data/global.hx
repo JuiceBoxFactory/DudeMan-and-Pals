0e56ed10b0015e8e0cc0281838d3fe7e59580e64
NeonExists <mehbeingrandom@gmail.com>
2024-03-18 08:23:01 -0600

    autoload

--- assets/data/global.hx
+++ assets/data/global.hx
@@ -1,3 +1,10 @@
-//
-function update(elapsed:Float)
-    if (FlxG.keys.justPressed.F5) FlxG.resetState();
+public var sectionselected = 0;
+
+function new() {
+    window.title = 'DudeMan and... PALS???';
+    window.setIcon(Image.fromBytes(File.getBytes('mods/DudeMan/data/modIcon.png')));
+}
+function update(elapsed) {
+    if (FlxG.keys.justPressed.F5)
+        FlxG.resetState();
+        }
