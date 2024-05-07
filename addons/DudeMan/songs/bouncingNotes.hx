eb7dabd8702f9a017c5e79747ba3ab7bb5c01ffb
NeonExists <91714683+MeltyKelpy@users.noreply.github.com>
2024-05-06 23:03:31 -0500

    ough note...myfortskines...

--- addons/DudeMan/songs/bouncingNotes.hx
+++ addons/DudeMan/songs/bouncingNotes.hx
@@ -1,5 +1,41 @@
 // real codes lmao
 // also use direction to do it since noteData is broken, FUCK!
+function postCreate() {
+	for (i in 0...4) {
+		player.members[i].scale.set(2.2, 2.2);
+		cpu.members[i].scale.set(2.2, 2.2);
+		player.members[i].antialiasing = false;
+		cpu.members[i].antialiasing = false;
+	}
+}
+
+function onPostStrumCreation(event) {
+
+	event.note.antialiasing = false;
+
+}
+
+function onPostNoteCreation(event) {
+
+	event.note.scale.set(2.2, 2.2);
+
+	event.note.antialiasing = false;
+	
+}
+
+function onNoteHit(event) {
+	if (event.showSplash) {
+	  event.showSplash = false;
+	  splashHandler.__grp = splashHandler.getSplashGroup(event.note.splash);
+	  var splash = splashHandler.__grp.showOnStrum(event.note.__strum);
+	  splash.shader = shader;
+	  splashHandler.add(splash);
+  
+	  while (splashHandler.members.length > 8)
+		splashHandler.remove(splashHandler.members[0], true);
+	}
+  }
+
 function onPlayerHit(e){
 
 	if (FlxG.save.data.notebounce == true && !player.cpu) {
