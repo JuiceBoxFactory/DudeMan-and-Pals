67911a6a46d194f34575e1d90aa6e3bcba1e6141
NeonExists <91714683+MeltyKelpy@users.noreply.github.com>
2024-05-18 19:27:36 -0500

    it does work

--- addons/DudeMan/data/events/middleCam.hx
+++ addons/DudeMan/data/events/middleCam.hx
@@ -1,19 +1,20 @@
 var posToBe = boyfriend.x + dad.x;
 var speed = 1;
+var prevLerp = 0.04;
 
 function onEvent(e) {
     if (e.event.name == "middleCam") {
 
-        var prevLerp = FlxG.camera.followLerp; 
-
-        posToBe = boyfriend.x + dad.x;
-        posToBe = posToBe / 2;
-        trace(posToBe);
-
-        var howFast = prevLerp * 30;
-        speed = howFast * e.event.params[1];
-
         if (e.event.params[0] == true) {
+            prevLerp = FlxG.camera.followLerp;
+
+            posToBe = boyfriend.x + dad.x;
+            posToBe = posToBe / 2;
+            trace(posToBe);
+    
+            var howFast = prevLerp * 30;
+            speed = howFast * e.event.params[1];
+
             FlxG.camera.followLerp = 0;
             FlxTween.tween(camGame.scroll, {x: posToBe}, speed, {ease: FlxEase.quartOut});      
         }
