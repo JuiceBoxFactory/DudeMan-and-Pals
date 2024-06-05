6dc90b1b89bf0157c834d61474ce4190110f4245
MeltyKelpy <mehbeingrandom@gmail.com>
2024-06-04 22:58:39 -0600

    no more buttons

--- addons/DudeMan/data/discord.hx
+++ addons/DudeMan/data/discord.hx
@@ -10,13 +10,4 @@ function onReady() {
 
 function onDiscordPresenceUpdate(e) {
 	var data = e.presence;
-
-	if(data.button1Label == null)
-		data.button1Label = "DOWNLOAD MY MOD NOW - dudeman";
-	if(data.button1Url == null)
-		data.button1Url = "https://gamebanana.com/mods/518286";
-	if(data.button2Label == null)
-		data.button2Label = "wait follow our twitter pllssss";
-	if(data.button2Url == null)
-		data.button2Url = "https://x.com/JuiceBoxFactory";
 }
